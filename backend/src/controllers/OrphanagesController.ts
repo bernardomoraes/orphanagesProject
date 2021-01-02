53c69ec9a057e9d586e96dcafc9bb74950a328ea
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2021-01-01 21:19:26 -0300

    WIP

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -38,7 +38,7 @@ export default {
         const requestImages = request.files as Express.Multer.File[];
 
         const images = requestImages.map(image => {
-            
+            return { path: image.path}
         })
 
 
---
+++ web 2/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles/global.css';
+
+import Routes from './routes';
+
+//JSX -> Javascript XML
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
+
+
